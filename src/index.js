eb5ff37288b4a9e0555ed0e838934269ab7bf6f7
ayema <ayema.chowdhury@gmail.com>
2021-08-02 12:25:17 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import { Frame } from 'chrome-sidebar'
+import { url } from './setting'
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+if (Frame.isReady()) {
+  Frame.toggle()
+} else {
+  boot()
+}
+
+function boot() {
+  const root = document.createElement('div')
+  document.body.appendChild(root)
+
+  const App = (
+    <Frame url={url} />
+  )
+
+  ReactDOM.render(App, root)
+}
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/setting.js
@@ -0,0 +1,7 @@
+export const url = 'https://dash-dev.grapevine.ai/'
+export const hosts = 'dash-dev.grapevine.ai/'
+export const iframeHosts = 'https://dash-dev.grapevine.ai/'
+
+export default {
+    hosts, iframeHosts, url
+}
